6f59e176505177b1eea43f21b90a9f93da29c7a5
Pedro Leite <pk.bleite@gmail.com>
2024-08-01 23:37:25 -0300

    first profile layout | Primeiro layout do perfil

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import React, { useState } from 'react'; 
 import myProfile from './assets/profile_images/myProfile.jpeg'
 import ProfileLoading from './components/profile.loading'
+import './styles/index.css'
+import './styles/desktop/desktop_landscape.css'
+import './styles/desktop/desktop_portrait.css'
+import './styles/mobile/mobile.portrait.css'
+import './styles/mobile/mobile_landscape.css'
+import './styles/mobile/mobile.portrait.css'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
--- src/components/profile.loading.tsx
+++ src/components/profile.loading.tsx
@@ -1,19 +1,18 @@
-import '../styles/App.css'
 import myProfile from '../assets/profile_images/myProfile.jpeg'
 
 function ProfileLoading() {
   
   return (
-    <>
-      <div className='introduction__page'>
-        <div className="loading__screen__5s">
-          <picture className="glowing__profile">
-            <img className='profile__image__area glow__area' src={myProfile} alt="Pedro's own photo" />
-            
-          </picture>
-        </div>
+    <div className='introduction__page'>
+      <div className="loading__screen__5s profile__borders">
+        <picture className="glowing__profile">
+          <img className='profile__image__area glow__area' src={myProfile} alt="Pedro's own photo" />
+        </picture>
       </div>
-    </>
+      <header className='welcoming__header'>
+        <h1>Olá, seja bem-vindo ao meu portfolio.</h1>
+      </header>
+    </div>
   )
 }
 
